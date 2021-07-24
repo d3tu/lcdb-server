ca017a69051485ec666e1b9affe687818b346c28
jpafly <69614422+d3tu@users.noreply.github.com>
2021-07-24 11:18:52 +0000

    .

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const lcdb = require("lcdb"),
-  { Server } = require("ws"),
-  dbs = {},
-  times = new Map();
+const lcdb = require('lcdb'),
+	{ Server } = require('ws'),
+	dbs = {},
+	times = new Map();
 
-module.exports = ({
-  cacheTimeout = 15000,
-  auth,
-  wsOptions,
-  port
-} = {}) => {
-  const server = new Server(Object.assign({}, wsOptions, { port }));
-  
-  setInterval(() => times.forEach((value, key) => {
-    if (Date.now() - value >= cacheTimeout) {
-      times.delete(key);
-      
-      delete dbs[key];
-    }
-  }), cacheTimeout);
-  
-  server.on("connection", client => {
-    client.on("message", message => {
-      const {
-        id, db, options, method, ref, value, op, auth: pass
-      } = JSON.parse(body);
-      
-      if (op === "login") {
-        if (pass === auth) return client.send("CONNECTED");
-        else return client.close();
-      }
-      
-      if (!dbs[db]) dbs[db] = lcdb(db, options);
-      
-      client.send(JSON.stringify({
-        id,
-        data: dbs[db][method] ? dbs[db][method](ref, value) : null
-      }));
-    });
-  });
+module.exports = ({ cacheTimeout = 15000, auth, wsOptions } = {}) => {
+	const server = new Server(wsOptions);
+
+	setInterval(
+		() =>
+			times.forEach((value, key) => {
+				if (Date.now() - value >= cacheTimeout) {
+					times.delete(key);
+
+					delete dbs[key];
+				}
+			}),
+		cacheTimeout
+	);
+
+	server.on('connection', client => {
+		client.on('message', message => {
+			const {
+				id,
+				db,
+				options,
+				method,
+				ref,
+				value,
+				op,
+				auth: pass
+			} = JSON.parse(body);
+
+			if (op === 'login') {
+				if (pass === auth) return client.send('CONNECTED');
+				else return client.close();
+			}
+
+			if (!dbs[db]) dbs[db] = lcdb(db, options);
+
+			client.send(
+				JSON.stringify({
+					id,
+					data: dbs[db][method] ? dbs[db][method](ref, value) : null
+				})
+			);
+		});
+	});
 };
